736c776e4337bd42544030229aa8e359d1132670
SaiChintada <chintadasai51758@gmail.com>
2026-05-09 21:42:52 +0530

    Fixed imports

--- backend/crud/transaction_crud.py
+++ backend/crud/transaction_crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
-from backend.models.transaction import Transaction
-from backend.schemas.transaction_schema import TransactionCreate
+from models.transaction import Transaction
+from schemas.transaction_schema import TransactionCreate
 
 
 def create_transaction(
