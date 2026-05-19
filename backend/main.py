ba8f08cf27c4743c51a57b11c7770e11d123285d
SaiChintada <chintadasai51758@gmail.com>
2026-05-19 16:44:43 +0530

    JWT auth and backend routing fixes

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from database import Base, engine
-
-from routes.transaction_routes import router as transaction_router
 from routes.auth_routes import router as auth_router
+from routes.transaction_routes import router as transaction_router
 
-# CREATE TABLES
-Base.metadata.create_all(bind=engine)
-
-# FASTAPI APP
 app = FastAPI()
 
-# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://your-vercel-app.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ROUTES
-app.include_router(transaction_router)
-
 app.include_router(auth_router)
 
+app.include_router(
+    transaction_router
+)
 
 @app.get("/")
-def root():
+def home():
     return {
         "message": "Expense Tracker API Running"
     }
